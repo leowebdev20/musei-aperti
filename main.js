f701033cafd02d99f067e6206dceefe1d5b066a2
leowebdev20 <serlebeiso@gmail.com>
2020-06-04 23:03:18 +0200

    04/06 commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// quel  && searchField.length !== 0 è mio zio!
+// funzione per richiamare l'elenco regioni come suggerimento barra di ricerca
 $(document).ready(function () {
     $.ajaxSetup({ cache: false });
     $('#search').keyup(function () {
@@ -6,8 +6,9 @@ $(document).ready(function () {
         $('#state').val('');
         var searchField = $('#search').val();
         var expression = new RegExp(searchField, "i");
-        $.getJSON('lista.json', function (data) {
+        $.getJSON('./data/lista.json', function (data) {
             $.each(data, function (key, value) {
+                // && searchField.length !== 0 è per evitare che i risultati escano quando il campo di ricerca è vuoto
                 if (value.regione.search(expression) != -1 && searchField.length !== 0) {
                     $('#result').append('<li class="list-group-item link-class"><img src="' + value.image + '" height="30" width="30" class="img-thumbnail" /> ' + '<span class="text-muted">' + value.regione + '</span>' + ' | <span class="text-muted"> Musei: ' + value.totmusei + '</span></li>');
                 }
@@ -23,15 +24,15 @@ $(document).ready(function () {
 });
 
 
-
+// funzione per indirizzare l'user alla pagina della regione cercata
 function redirect() {
     var searchitem = document.getElementById("search");
-    window.location.replace("http://localhost:4000/phptest/index-" + searchitem.value + ".php");
+    window.location.replace("http://127.0.0.1:5500/index-" + searchitem.value + ".html");
 }
 
-
+// funzione per ricevere dal json i dati della dati-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
-    $.getJSON("data/dati-table2.json", function (data) {
+    $.getJSON("data/dati-table.json", function (data) {
         var numeri = '';
         $.each(data, function (key, value) {
             numeri += '<tr>';
@@ -42,6 +43,8 @@ $(document).ready(function () {
         $('#dati-table').append(numeri);
     });
 });
+
+// funzione per ricevere dal json i dati della regioni-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/regioni-table.json", function (data) {
         var numeri = '';
@@ -54,13 +57,15 @@ $(document).ready(function () {
         $('#regioni-table').append(numeri);
     });
 });
+
+// funzione per ricevere dal json i dati della recenti-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/recenti-table.json", function (data) {
         var numeri = '';
         $.each(data, function (key, value) {
             numeri += '<tr>';
             numeri += '<td>' + value.museo + '</td>';
-            numeri += '<td>' + value.luogo + '</td>';
+            numeri += '<td>' + value.citta + '</td>';
             numeri += '<td>' + value.aperto + '</td>';
             numeri += '<td>' + value.programmata + '</td>';
             numeri += '</tr>';
@@ -68,13 +73,15 @@ $(document).ready(function () {
         $('#recenti-table').append(numeri);
     });
 });
+
+// funzione per ricevere dal json i dati della programmate-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/programmate-table.json", function (data) {
         var numeri = '';
         $.each(data, function (key, value) {
             numeri += '<tr>';
             numeri += '<td>' + value.museo + '</td>';
-            numeri += '<td>' + value.luogo + '</td>';
+            numeri += '<td>' + value.citta + '</td>';
             numeri += '<td>' + value.aperto + '</td>';
             numeri += '<td>' + value.programmata + '</td>';
             numeri += '</tr>';
@@ -82,20 +89,37 @@ $(document).ready(function () {
         $('#programmate-table').append(numeri);
     });
 });
+
+// funzione per ricevere dal json i dati della veneto-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
-    $.getJSON("data/venezia-table.json", function (data) {
+    $.getJSON("data/veneto-table.json", function (data) {
         var numeri = '';
         $.each(data, function (key, value) {
+            let apertura;  //qui metto che se il dato è non definito ritorno un messaggio altrimenti ritorno il dato
+            if (value.aperto === undefined) {
+                apertura = "Se lo sai scrivici!";
+            } else {
+                apertura = value.aperto;
+            };
+            let programmazione;  //qui metto che se il dato è non definito ritorno un messaggio altrimenti ritorno il dato
+            if (value.programmata === undefined) {
+                programmazione = "Se lo sai scrivici!";
+            } else {
+                programmazione = value.programmata;
+            };             // parte del getting the data from the json for the table
             numeri += '<tr>';
             numeri += '<td>' + value.museo + '</td>';
-            numeri += '<td>' + value.luogo + '</td>';
-            numeri += '<td>' + value.aperto + '</td>';
-            numeri += '<td>' + value.programmata + '</td>';
+            numeri += '<td>' + value.citta + '</td>';
+            numeri += '<td>' + apertura + '</td>';
+            numeri += '<td>' + programmazione + '</td>';
             numeri += '</tr>';
+
         });
-        $('#venezia-table').append(numeri);
+        $('#veneto-table').append(numeri);
     });
 });
+
+// funzione per ricevere dal json i dati della lazio-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/lazio-table.json", function (data) {
         var numeri = '';
@@ -125,53 +149,226 @@ $(document).ready(function () {
 });
 
 
+// funzione per il sorting lazio-table  
+function sortLazio(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("lazio-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+};
 
+// funzione per il sorting veneto-table  
+function sortVeneto(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("veneto-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+};
 
+// funzione per il sorting recenti-table  
+function sortRecenti(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("recenti-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+};
 
-
-
-
-
-
-
-
-
-// const datiTable = document.querySelector('#dati-table > tbody');
-
-// function loadDati() {
-//     const request = new XMLHttpRequest();
-
-//     request.open("get", "data/dati-table.json");
-//     request.onload = () => {
-//         try {
-//             const json = JSON.parse(request.responseText);
-//             populateDati(json);
-//         } catch (e) {
-//             console.warn("Could not load dati table!");
-//         }
-//     };
-
-//     request.send();
-// }
-
-// function populateDati(json) {
-//     //clears out existing table data
-//     while (datiTable.firstChild) {
-//         datiTable.removeChild(datiTable.firstChild);
-//     }
-
-//     //populate table
-//     json.forEach((row) => {
-//         const tr = document.createElement("tr");
-
-//         row.forEach((cell) => {
-//             const td = document.createElement("td");
-//             td.textContent = cell;
-//             tr.appendChild(td);
-//         });
-
-//         datiTable.appendChild(tr);
-//     });
-// }
-
-// document.addEventListener("DOMContentLoaded", () => { loadDati(); })
+// funzione per il sorting programmate-table  
+function sortProgrammate(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("programmate-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+};
