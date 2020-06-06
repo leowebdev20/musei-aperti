897b394954d541200f69c8c22c975035074ec108
leowebdev20 <serlebeiso@gmail.com>
2020-06-06 18:46:35 +0200

    single page commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// funzione per richiamare l'elenco regioni come suggerimento barra di ricerca
+// live search (richiamare elenco regioni come suggerimento barra di ricerca)
 $(document).ready(function () {
     $.ajaxSetup({ cache: false });
     $('#search').keyup(function () {
@@ -23,14 +23,67 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#results-container").hide();
+    function search() {
+        $.ajaxSetup({ cache: false });
+        $('#search').keyup(function () {
+            $('#state').val('');
+            var searchField = $('#search').val();
+            var expression = new RegExp(searchField, "i");
+            $.getJSON('./data/veneto-table.json', function (data) {
+                var numeri = '';
+                $.each(data, function (key, value) {
+                    let apertura;  //qui metto che se il dato è non definito ritorno un messaggio altrimenti ritorno il dato
+                    if (value.aperto === undefined) {
+                        apertura = "Se lo sai scrivici!";
+                    } else {
+                        apertura = value.aperto;
+                    };
+                    let programmazione;  //qui metto che se il dato è non definito ritorno un messaggio altrimenti ritorno il dato
+                    if (value.programmata === undefined) {
+                        programmazione = "Se lo sai scrivici!";
+                    } else {
+                        programmazione = value.programmata;
+                    };             // parte del getting the data from the json for the table
+                    if (searchField.toLowerCase() === "veneto") {
+                        numeri += '<tr>';
+                        numeri += '<td>' + value.museo + '</td>';
+                        numeri += '<td>' + value.citta + '</td>';
+                        numeri += '<td>' + apertura + '</td>';
+                        numeri += '<td>' + programmazione + '</td>';
+                        numeri += '</tr>';
+                    }
+                });
+                $('#results-table').append(numeri);
+            });
+        });
+    }
 
-// funzione per indirizzare l'user alla pagina della regione cercata
-function redirect() {
-    var searchitem = document.getElementById("search");
-    window.location.replace("http://127.0.0.1:5500/index-" + searchitem.value + ".html");
-}
+    $("#button").click(function () {
+        $(".containerZ").hide();
+        $("#result").hide();
+        search();
+        $("#results-container").show();
+    });
+    $('#search').keyup(function (e) {
+        if (e.keyCode == 13) {
+            $(".containerZ").hide();
+            $("#result").hide();
+            search();
+            $("#results-container").show();
+        }
+    });
+});
 
-// funzione per ricevere dal json i dati della dati-table e mostrarli nella rispettiva html table
+// // indirizzare l'user alla pagina della regione cercata - disattivato vista l'inutilita con github pages / netlify
+// function redirect() {
+//     var searchitem = document.getElementById("search");
+//     window.location.replace("http://127.0.0.1:5500/index-" + searchitem.value + ".html");
+// }
+
+// ricevere dal json i dati della dati-table e mostrarli nella rispettiva html table
+
 $(document).ready(function () {
     $.getJSON("data/dati-table.json", function (data) {
         var numeri = '';
@@ -44,7 +97,7 @@ $(document).ready(function () {
     });
 });
 
-// funzione per ricevere dal json i dati della regioni-table e mostrarli nella rispettiva html table
+// ricevere dal json i dati della regioni-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/regioni-table.json", function (data) {
         var numeri = '';
@@ -58,7 +111,7 @@ $(document).ready(function () {
     });
 });
 
-// funzione per ricevere dal json i dati della recenti-table e mostrarli nella rispettiva html table
+// ricevere dal json i dati della recenti-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/recenti-table.json", function (data) {
         var numeri = '';
@@ -74,7 +127,7 @@ $(document).ready(function () {
     });
 });
 
-// funzione per ricevere dal json i dati della programmate-table e mostrarli nella rispettiva html table
+// ricevere dal json i dati della programmate-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/programmate-table.json", function (data) {
         var numeri = '';
@@ -90,7 +143,7 @@ $(document).ready(function () {
     });
 });
 
-// funzione per ricevere dal json i dati della veneto-table e mostrarli nella rispettiva html table
+// ricevere dal json i dati della veneto-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/veneto-table.json", function (data) {
         var numeri = '';
@@ -119,7 +172,7 @@ $(document).ready(function () {
     });
 });
 
-// funzione per ricevere dal json i dati della lazio-table e mostrarli nella rispettiva html table
+// ricevere dal json i dati della lazio-table e mostrarli nella rispettiva html table
 $(document).ready(function () {
     $.getJSON("data/lazio-table.json", function (data) {
         var numeri = '';
@@ -149,7 +202,7 @@ $(document).ready(function () {
 });
 
 
-// funzione per il sorting lazio-table  
+// sorting lazio-table  
 function sortLazio(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("lazio-table");
@@ -205,7 +258,7 @@ function sortLazio(n) {
     }
 };
 
-// funzione per il sorting veneto-table  
+// sorting veneto-table  
 function sortVeneto(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("veneto-table");
@@ -261,7 +314,7 @@ function sortVeneto(n) {
     }
 };
 
-// funzione per il sorting recenti-table  
+// sorting recenti-table  
 function sortRecenti(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("recenti-table");
@@ -317,10 +370,65 @@ function sortRecenti(n) {
     }
 };
 
-// funzione per il sorting programmate-table  
+// sorting programmate-table  
 function sortProgrammate(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("programmate-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+};
+// sorting results-table 
+function sortResults(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("results-table");
     switching = true;
     //Set the sorting direction to ascending:
     dir = "asc";
